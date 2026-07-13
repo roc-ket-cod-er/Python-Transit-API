2c0d17758824bb56eff4db13277e9dd7f7c33fc6
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-07-13 16:15:11 -0400

    Update routing.py

--- src/routing/routing.py
+++ src/routing/routing.py
@@ -1,12 +1,23 @@
 import transit
 import walking
+import gtfs
 
 
 def route(start: tuple[float, float], end: tuple[float, float]):
     trip = transit.a_to_b(start, end)[0]
-    starting_stop, ending_stop, trip_id = trip
+    starting_stop, ending_stop, trip_id, agency = trip
 
-    print(starting_stop, ending_stop, trip_id)
+    for stop in gtfs.all_stops:
+        if stop["id"] == starting_stop:
+            starting_stop_name = stop["name"]
+        elif stop["id"] == ending_stop:
+            ending_stop_name = stop["name"]
+
+    route = []
+
+    print(f"from {starting_stop_name} to {ending_stop_name}, use trip number {trip_id} (run by {agency})")
 
 if __name__ == '__main__':
+    print("\n\n\n\n")
     route((43.498310, -80.529517), (43.462290, -80.523532))
+    print("\n\n\n\n")
