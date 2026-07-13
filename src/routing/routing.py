b4aa6e21f907ec18480924637fdf4c330f938e06
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-07-13 15:52:55 -0400

    Update routing.py

--- src/routing/routing.py
+++ src/routing/routing.py
@@ -5,3 +5,8 @@ import walking
 def route(start: tuple[float, float], end: tuple[float, float]):
     trip = transit.a_to_b(start, end)[0]
     starting_stop, ending_stop, trip_id = trip
+
+    print(starting_stop, ending_stop, trip_id)
+
+if __name__ == '__main__':
+    route((43.498310, -80.529517), (43.462290, -80.523532))
