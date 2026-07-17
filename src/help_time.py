cb7f57b4c8c374920fb46d47f040a12234473cf7
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-07-17 12:52:45 -0400

    Create help_time.py

---
+++ src/help_time.py
@@ -0,0 +1,28 @@
+import time
+
+tm_year = 0
+tm_mon = 1
+tm_mday = 2
+tm_hour = 3
+tm_min = 4
+tm_sec = 5
+
+def hms(seconds: int) -> tuple[int, int, int]:
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    return hours, minutes, seconds
+
+def seconds(t: tuple[int, int, int]) -> int:
+    return t[0] * 3600 + t[1] * 60 + t[2]
+
+def time_rn(offset: tuple=(0,0,0)):
+    t = time.localtime()
+
+    ctime = [
+        f"{ t[tm_hour]+offset[0] }:{ t[tm_min]+offset[1] }:{ t[tm_sec]+offset[2] }",
+        f"{ t[tm_year] }{ t[tm_mon] :02d}{ t[tm_mday] }",
+        (t[tm_hour]+offset[0], t[tm_min]+offset[1], t[tm_sec]+offset[2])
+    ]
+
+    return ctime
